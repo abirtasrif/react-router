23a8b0ada8ce8c7504371c292ccc23db13aa93e6
abirtasrif <abirtasrif@gmail.com>
2022-12-12 23:41:02 +0600

    improving

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
+  <Router>
+    <React.StrictMode>
       <App />
-    </Router>
-  </React.StrictMode>
+    </React.StrictMode>
+  </Router>
 );
